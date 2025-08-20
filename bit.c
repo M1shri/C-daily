f74aaa7ef5caa289f29009f17b76e50ae1553e8f
M1shri <krish.mishra745@gmail.com>
2025-08-20 19:01:25 +0530

    .

---
+++ bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a = 5;  
+    int b = 3;   
+
+    // Bitwise AND
+    printf("a & b  = %d\n", a & b);
+
+    // Bitwise OR
+    printf("a | b  = %d\n", a | b);
+
+    // Bitwise XOR
+    printf("a ^ b  = %d\n", a ^ b);
+
+    // Bitwise NOT
+    printf("~a     = %d\n", ~a);
+    printf("~b     = %d\n", ~b);
+}

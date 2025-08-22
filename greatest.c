0ff365efccd5dcb130311b6d9ff1bdc1f49ca6e7
M1shri <krish.mishra745@gmail.com>
2025-08-22 14:45:28 +0530

    .

---
+++ greatest.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter three values - ");
+    scanf("\n %d \n%d \n%d",&a ,&b ,&c);
+    int greatest = (a>b) ? ((a>c) ? a : c) : (c>b) ? c : b;
+    printf ("%d",greatest);
+}

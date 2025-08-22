751702e752d3d08189181a78e879e39317707506
M1shri <krish.mishra745@gmail.com>
2025-08-22 14:26:17 +0530

    .

--- bit.c
+++ bit.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
+void and(int a , int b);
+void or(int a , int b);
+void xor(int a , int b);
+void not(int a , int b);
 
 int main() 
 {
     int a = 5;  
     int b = 3;   
 
-    // Bitwise AND
-    printf("a & b  = %d\n", a & b);
+ and(a, b); // Bitwise and
 
-    // Bitwise OR
-    printf("a | b  = %d\n", a | b);
+  or(a, b);  // Bitwise OR
 
-    // Bitwise XOR
-    printf("a ^ b  = %d\n", a ^ b);
+  xor(a, b);  // Bitwise XOR
+    
+  not(a, b); // Bitwise NOT
+}
 
-    // Bitwise NOT
+void and(int a, int b){ // Bitwise and
+    printf("a & b  = %d\n", a & b);}
+
+void or(int a,int b)  // Bitwise OR
+    {printf("a | b  = %d\n", a | b);}
+
+ void xor(int a,int b)  // Bitwise XOR
+    { printf("a ^ b  = %d\n", a ^ b);}
+
+void not(int a, int b) { // Bitwise NOT
     printf("~a     = %d\n", ~a);
     printf("~b     = %d\n", ~b);
-}
+  }

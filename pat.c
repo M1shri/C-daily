bfb2c43eff66f586be6fcb83c893689fd2f454a8
M1shri <krish.mishra745@gmail.com>
2025-09-01 14:50:22 +0530

    .

--- pat.c
+++ pat.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    printf("    *   \n");
-    printf("   * * \n");
-    printf("  * * * \n");
-    printf(" * * * * \n");
-    printf("* * * * *  \n");
+    int n= 5;
+    for(int i=1; i<=n;i++)
+    {
+        for(int j =i; j <= n; j++)
+        printf(" ");
+ 
+        for(int k=1; k<=i;k++ )
+        {
+        printf("* ");
+        }
+    printf("\n");
+    }
+    for(int i = n - 1; i >= 1; i--)
+    {
+        for(int j = n; j >= i; j--)
+            printf(" ");
+        for(int k = 1; k <= i; k++)
+            printf("* ");
+        printf("\n");
+    }
 }

8f914f1551125a76b581bbf45b2098c858939d25
M1shri <krish.mishra745@gmail.com>
2025-09-09 23:33:06 +0530

    .

--- pp2.c
+++ pp2.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
 #include <math.h>
-
-int main() {
-    float a, b, c, d, root1, root2, real, imag;
-    printf("Enter coefficients a, b, c: ");
-    scanf("%f %f %f", &a, &b, &c);
-
-    d = b*b - 4*a*c;
-
-    if (d > 0) {
-        root1 = (-b + sqrt(d)) / (2*a);
-        root2 = (-b - sqrt(d)) / (2*a);
-        printf("Roots are real and distinct: %.2f and %.2f\n", root1, root2);
-    }
-    else if (d == 0) {
-        root1 = -b / (2*a);
-        printf("Roots are real and equal: %.2f and %.2f\n", root1, root1);
-    }
-    else {
-        real = -b / (2*a);
-        imag = sqrt(-d) / (2*a);
-        printf("Roots are complex: %.2f + %.2fi and %.2f - %.2fi\n", real, imag, real, imag);
-    }
+int main() 
+{
+    int choice, num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("\nMenu:\n");
+    printf("1. Check Even or Odd\n");
+    printf("2. Check Positive or Negative\n");
+    printf("3. Print Square\n");
+    printf("4. Print Square Root\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    switch (choice) 
+{        case 1:
+            if (num % 2 == 0)
+                printf("%d is Even\n", num);
+            else
+                printf("%d is Odd\n", num);
+            break;
+         case 2:
+            if (num > 0)
+                printf("%d is Positive\n", num);
+            else if (num < 0)
+                printf("%d is Negative\n", num);
+            else
+                printf("Number is Zero\n");
+            break;
+        case 3:
+            printf("Square of %d is %d\n", num, num * num);
+            break;
+        case 4:
+            if (num >= 0)
+                printf("Square Root of %d is %.2f\n", num, 		sqrt(num));
+            else
+                printf("Square root of negative number is not 	real.\n");
+            break;
+        default:
+            printf("Invalid choice.\n");
+}
     return 0;
 }

1f06367deb9c9278c9384a77b845d265a54c52d1
M1shri <krish.mishra745@gmail.com>
2025-09-10 15:00:08 +0530

    .

--- pp2.c
+++ pp2.c
@@ -1,43 +1,13 @@
 #include <stdio.h>
-#include <math.h>
-int main() 
-{
-    int choice, num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    printf("\nMenu:\n");
-    printf("1. Check Even or Odd\n");
-    printf("2. Check Positive or Negative\n");
-    printf("3. Print Square\n");
-    printf("4. Print Square Root\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-    switch (choice) 
-{        case 1:
-            if (num % 2 == 0)
-                printf("%d is Even\n", num);
-            else
-                printf("%d is Odd\n", num);
-            break;
-         case 2:
-            if (num > 0)
-                printf("%d is Positive\n", num);
-            else if (num < 0)
-                printf("%d is Negative\n", num);
-            else
-                printf("Number is Zero\n");
-            break;
-        case 3:
-            printf("Square of %d is %d\n", num, num * num);
-            break;
-        case 4:
-            if (num >= 0)
-                printf("Square Root of %d is %.2f\n", num, 		sqrt(num));
-            else
-                printf("Square root of negative number is not 	real.\n");
-            break;
-        default:
-            printf("Invalid choice.\n");
-}
+
+int main() {
+    float km, meters, feet, inches, cm;
+    printf("Enter distance in kilometers: ");
+    scanf("%f", &km);
+    meters = km * 1000;
+    cm = km * 100000;
+    feet = km * 3280.84;
+    inches = km * 39370.1;
+    printf("Meters: %.2f\nCentimeters: %.2f\nFeet: %.2f\nInches: %.2f\n", meters, cm, feet, inches);
     return 0;
 }

807d0fbe394008c7b54aa84c4706596221083e7b
M1shri <krish.mishra745@gmail.com>
2025-10-06 22:31:21 +0530

    j

--- pattern.c
+++ pattern.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    for(int i= 5; i>=1; i--)
+    int n= 5;
+    for(int i = n; i>=1; i--)
     {
-        for(int n=5; n>=i;n--)
-        {
-            printf("%d",n);
-        }
-    printf("\n");
+        for(int j=i-1; i>=1;j--)
+        printf(" ");
+
+        for(int k=1; k<=2*i-1; k++)
+        printf("*");
+
+        printf("\n");
     }
 }

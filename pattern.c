b6c52c745a0b0e3e253694b66781a9f9afe91745
M1shri <krish.mishra745@gmail.com>
2025-10-08 00:03:13 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+
+/*
+      *   --- 3 space , odd star (1)
+    * * * ---- 2 space , odd star (3)
+  * * * * *
+*/
+
 int main()
 {
-    int n= 5;
-    for(int i = n; i>=1; i--)
+    for(int i = 1; i<=5; i++)
     {
-        for(int j=i-1; i>=1;j--)
-        printf(" ");
+        for(int j=5-i; j>=1;j--){
+            printf(" ");
+        }
 
-        for(int k=1; k<=2*i-1; k++)
-        printf("*");
-
+        for(int k = 1; k <= 2*i-1;k++){
+            printf("*");
+        }
         printf("\n");
     }
 }

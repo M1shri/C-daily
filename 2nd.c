afbde9be5b266673c93a38fdec330f9afb66f8b5
M1shri <krish.mishra745@gmail.com>
2025-12-22 01:20:46 +0530

    .

--- 2nd.c
+++ 2nd.c
@@ -3,17 +3,30 @@ int main()
 {
     int arr[5];
     int greatest=0;
+    int grts;
     printf("enter values");
     for(int i=0;i<5;i++)
     {
         scanf("%d",&arr[i]);
     }
     greatest = arr[0];
-for(int i=0; i<=5; i++)
-{
+    for(int i=0; i<5; i++)
+    {
     if(arr[i]>greatest)
     greatest= arr[i];
+    grts = i;
     }   
-    printf("%d",greatest);
+    printf("%d \n",greatest);
+    int secg =0;
+
+    for(int i=0; i<5; i++)
+    {
+        if(i == grts)
+        continue;
+        else
+        if(arr[i]>=secg)
+        secg = arr[i];
+    }
+    printf("%d",secg);
 
 }
